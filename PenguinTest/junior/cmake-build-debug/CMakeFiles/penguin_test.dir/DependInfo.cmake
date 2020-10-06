a6f93e99d7c64fdcd3e2b3519364c2962d6cd864
ni-k-azakov <nani.l.desu.l.ka@gmail.com>
2020-10-06 21:42:35 +0300

    :beer: Update: build 1.0

---
+++ PenguinTest/junior/cmake-build-debug/CMakeFiles/penguin_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/just/git_stuff/c_cpp_stuff/PenguinTest/junior/src/TemplateText.cpp" "/home/just/git_stuff/c_cpp_stuff/PenguinTest/junior/cmake-build-debug/CMakeFiles/penguin_test.dir/src/TemplateText.cpp.o"
+  "/home/just/git_stuff/c_cpp_stuff/PenguinTest/junior/src/XMLBuilder.cpp" "/home/just/git_stuff/c_cpp_stuff/PenguinTest/junior/cmake-build-debug/CMakeFiles/penguin_test.dir/src/XMLBuilder.cpp.o"
+  "/home/just/git_stuff/c_cpp_stuff/PenguinTest/junior/src/main.cpp" "/home/just/git_stuff/c_cpp_stuff/PenguinTest/junior/cmake-build-debug/CMakeFiles/penguin_test.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PenguinTest/junior/cmake-build-debug/CMakeFiles/penguin_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/penguin_test.dir/src/TemplateText.cpp.o"
+  "CMakeFiles/penguin_test.dir/src/XMLBuilder.cpp.o"
+  "CMakeFiles/penguin_test.dir/src/main.cpp.o"
+  "penguin_test"
+  "penguin_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/penguin_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
